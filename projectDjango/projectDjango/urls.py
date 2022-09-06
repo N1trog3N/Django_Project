cefd38a8455a4897eab5f5867b73a1a1a1a6a208
N1trog3N <antoha323245@gmail.com>
2022-09-06 18:20:35 +0300

    Working (v.0.1)

--- projectDjango/projectDjango/urls.py
+++ projectDjango/projectDjango/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from projectApp import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  path('home/', views.home, name='Home'),
+path('about/', views.about, name='about'), path('', views.home, name='about')
 ]
